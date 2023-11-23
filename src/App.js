d33ead0aae0847d436bf32f931605fbb8495e012
Arely Gonzalez <arely.web@gmail.com>
2023-11-23 12:13:09 -0600

    creando el front de la app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div>
+      <h1>AZURE</h1>
+      <p>
+        Inserta la URL
+      </p>
+      <input type="text" placeholder="Enter URL" />
+      <button onClick={handleGenerate}>Generate</button>
+      <button onClick={handleAnalyze}>Analyze</button>
+    </div>
+  );
+}
+
+function handleGenerate() {
+  // Logic for generating
+}
+
+function handleAnalyze() {
+  // Logic for analyzing
 }
 
 export default App;
